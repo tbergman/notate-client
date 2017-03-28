074783496e9e59c51cf419899b440b50e932ec8f
Richard Plotkin <richardjplotkin@gmail.com>
2017-03-28 16:46:05 -0400

    add a couple more good dependencies

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+// @flow
+
+module.exports = function override(config: Object) {
+  //do stuff with the webpack config...
+  // if(config && config.resolve && config.resolve.alias) {
+  //   config.resolve.alias['joi'] = 'joi-browser'
+  // }
+  return config;
+}
