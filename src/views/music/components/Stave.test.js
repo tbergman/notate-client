5092b42ff2e982c7be6680b12b27534ecd64e9d5
Caio Kinzel Filho <caiokf@gmail.com>
2017-05-01 11:19:28 +0300

    adding a test setup file and removing unused jest config

--- src/views/music/components/Stave.test.js
+++ src/views/music/components/Stave.test.js
@@ -5,20 +5,6 @@ import { mount, ReactWrapper } from 'enzyme'
 import Stave from './Stave'
 
 function setup(): ReactWrapper {
-  const jsdom = require('jsdom').JSDOM
-
-  const exposedProperties = ['window', 'navigator', 'document']
-  global.window = new jsdom('<!DOCTYPE html><div id="root"/>').window
-  global.document = global.window.document
-  global.navigator = { userAgent: 'node.js' }
-
-  Object.keys(global.document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
-      exposedProperties.push(property)
-      global[property] = global.document.defaultView[property]
-    }
-  })
-
   return mount(<Stave/>)
 }
 
