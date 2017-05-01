65af70c7c8ba50b5c4450bdd3965d98c2a75f578
Richard Plotkin <richardjplotkin@gmail.com>
2017-05-01 15:21:49 -0400

    fixed flow issues

--- src/views/music/components/Stave.test.js
+++ src/views/music/components/Stave.test.js
@@ -5,6 +5,10 @@ import { mount } from 'enzyme'
 import { Flow } from 'vexflow'
 import Stave from './Stave'
 
+const mockAttributesNamedNodeMap = {
+  getNamedItem: jest.fn()
+}
+
 describe('stave component', () => {
   it('renders without crashing', () => {
     const component = mount(<Stave/>)
@@ -28,26 +32,29 @@ describe('stave component', () => {
   it('defaults height and width to 500', () => {
     const component = mount(<Stave/>)
     const element = component.getDOMNode()
-    const attrs = element.querySelector('svg').attributes
+    const svg = element.querySelector('svg')
+    const { attributes = mockAttributesNamedNodeMap } = svg || {}
 
-    expect(attrs['height'].value).toEqual('500')
-    expect(attrs['width'].value).toEqual('500')
+    expect(attributes.getNamedItem('height').value).toEqual('500')
+    expect(attributes.getNamedItem('width').value).toEqual('500')
   })
 
   it('sets the svg height', () => {
     const component = mount(<Stave height={100}/>)
     const element = component.getDOMNode()
-    const attrs = element.querySelector('svg').attributes
+    const svg = element.querySelector('svg')
+    const { attributes = mockAttributesNamedNodeMap } = svg || {}
 
-    expect(attrs['height'].value).toEqual('100')
+    expect(attributes.getNamedItem('height').value).toEqual('100')
   })
 
   it('sets the svg width', () => {
     const component = mount(<Stave width={100}/>)
     const element = component.getDOMNode()
-    const attrs = element.querySelector('svg').attributes
+    const svg = element.querySelector('svg')
+    const { attributes = mockAttributesNamedNodeMap } = svg || {}
 
-    expect(attrs['width'].value).toEqual('100')
+    expect(attributes.getNamedItem('width').value).toEqual('100')
   })
 
   it('defaults clef type to treble', () => {
