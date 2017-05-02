c346c531ff19442c73f3b6e1a7538e3b058e0596
Andy <accassid@buffalo.edu>
2017-05-02 16:37:26 -0400

    Added selector tests.

--- src/modules/toolbox/__tests__/toolbox.js
+++ src/modules/toolbox/__tests__/toolbox.js
@@ -3,6 +3,7 @@
 import toolboxReducer from '../reducers'
 import { SET_ACCIDENTAL, SET_DURATION } from '../actionTypes'
 import type { SetAccidentalAction, SetDurationAction } from '../actions'
+import { selectSelectedAccidental, selectSelectedDuration } from 'modules/toolbox/selectors'
 import { ACCIDENTAL, DURATION } from '../constants'
 
 describe('modules/toolbox', () => {
@@ -48,6 +49,50 @@ describe('modules/toolbox', () => {
         it('should treat current state as immutable', () => {
             let currentState = initialState;
             toolboxReducer(currentState, action);
+            expect(currentState).toEqual(initialState);
+        });
+    });
+
+    describe('SELECT_ACCIDENTAL', () => {
+        let initialState;
+        beforeEach(() => {
+            initialState = {
+                toolbox: {
+                    selectedDuration: DURATION.QUARTER,
+                    selectedAccidental: ACCIDENTAL.NATURAL,
+                }
+            }
+        })
+        it('should select the accidental from the current state', () => {
+            let currentState = initialState;
+            let selectedAccidental = selectSelectedAccidental(currentState);
+            expect(selectedAccidental).toEqual(ACCIDENTAL.NATURAL);
+        });
+        it('should treat current state as immutable', () => {
+            let currentState = initialState;
+            selectSelectedAccidental(currentState);
+            expect(currentState).toEqual(initialState);
+        });
+    });
+
+    describe('SELECT_DURATION', () => {
+        let initialState;
+        beforeEach(() => {
+            initialState = {
+                toolbox: {
+                    selectedDuration: DURATION.QUARTER,
+                    selectedAccidental: ACCIDENTAL.NATURAL,
+                }
+            }
+        })
+        it('should select the duration from the current state', () => {
+            let currentState = initialState;
+            let selectedAccidental = selectSelectedDuration(currentState);
+            expect(selectedAccidental).toEqual(DURATION.QUARTER);
+        });
+        it('should treat current state as immutable', () => {
+            let currentState = initialState;
+            selectSelectedDuration(currentState);
             expect(currentState).toEqual(initialState);
         });
     });
