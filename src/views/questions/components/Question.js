1b5809f134c32c4cf37dd6ff7e7ed97327585c57
Caio Kinzel Filho <caiokf@gmail.com>
2017-05-11 20:21:46 +0300

    answers & grading

--- src/views/questions/components/Question.js
+++ src/views/questions/components/Question.js
@@ -3,6 +3,7 @@ import type { Question as QuestionType } from 'modules/student-test'
 
 import React, { Component } from 'react'
 import Stave from 'views/music/components/Stave'
+import Grader from 'modules/grading/grader'
 
 type Props = {
   question: QuestionType,
@@ -11,11 +12,16 @@ type Props = {
 export default class Question extends Component {
   props: Props
 
+  grade() {
+    new Grader().grade(this.props.question.answers, this.props.question.student)
+  }
+
   render(): React.Element<any> {
     return (
       <div className="question">
         <div className="question-index">
           {this.props.question.index}
+          <input type="button" onClick={() => this.grade()} value="Grade" />
         </div>
         <div className="question-statement">
           {this.props.question.statement}
