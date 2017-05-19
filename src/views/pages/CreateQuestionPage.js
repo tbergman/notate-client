4557c42121adc5e8c74e4e4f0b238d9151bc4ab5
Caio Kinzel Filho <caiokf@gmail.com>
2017-05-19 16:32:32 +0300

    toolbox calling action creators

--- src/views/pages/CreateQuestionPage.js
+++ src/views/pages/CreateQuestionPage.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 import Layout from './Layout'
+import Toolbox from 'views/toolbox/Toolbox'
 
 class CreateQuestionPage extends Component {
   render(): React.Element<any> {
@@ -16,7 +17,7 @@ class CreateQuestionPage extends Component {
 
         <PageContainer>
           <ToolboxContainer>
-
+            <Toolbox />
           </ToolboxContainer>
           <QuestionContainer>
 
@@ -35,9 +36,10 @@ const PageContainer = styled.div`
 const ToolboxContainer = styled.div`
   flex: 3;
   box-shadow: 2px 0 5px -2px rgba(0, 0, 0, 0.2);
+  padding: 30px;
 `
 const QuestionContainer = styled.div`
   flex: 7
 `
 
-export default connect(state => {})(CreateQuestionPage)
+export default connect(state => ({}))(CreateQuestionPage)
---
+++ src/views/toolbox/Toolbox.js
@@ -0,0 +1,63 @@
+// @flow
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import _ from 'lodash'
+import styled from 'styled-components'
+import { setDuration, setAccidental } from 'modules/toolbox/actions'
+import { ACCIDENTAL, DURATION } from 'modules/toolbox'
+
+class Toolbox extends Component {
+  selectEraser() { }
+
+  render(): React.Element<any> {
+    return (
+      <ToolboxContainer>
+        <ToolboxItem bar onClick={() => this.props.setDuration(DURATION.EIGHTH)}>e</ToolboxItem>
+        <ToolboxItem bar onClick={() => this.props.setDuration(DURATION.QUARTER)}>q</ToolboxItem>
+        <ToolboxItem bar onClick={() => this.props.setDuration(DURATION.HALF)}>h</ToolboxItem>
+        <ToolboxItem bar onClick={() => this.props.setDuration(DURATION.WHOLE)}>w</ToolboxItem>
+        <ToolboxItem onClick={() => this.selectEraser()}>D</ToolboxItem>
+        <ToolboxSeparator />
+        <ToolboxItem bar onClick={() => this.props.setAccidental(ACCIDENTAL.NATURAL)}>N</ToolboxItem>
+        <ToolboxItem bar onClick={() => this.props.setAccidental(ACCIDENTAL.NATURAL)}>NT</ToolboxItem>
+        <ToolboxItem bar onClick={() => this.props.setAccidental(ACCIDENTAL.SHARP)}>#</ToolboxItem>
+        <ToolboxItem bar onClick={() => this.props.setAccidental(ACCIDENTAL.DOUBLE_SHARP)}>##</ToolboxItem>
+        <ToolboxItem bar onClick={() => this.props.setAccidental(ACCIDENTAL.FLAT)}>b</ToolboxItem>
+        <ToolboxItem onClick={() => this.props.setAccidental(ACCIDENTAL.DOUBLE_FLAT)}>bb</ToolboxItem>
+      </ToolboxContainer>
+    )
+  }
+}
+
+const ToolboxContainer = styled.div`
+  border: 1px solid #dce0e0;
+  border-radius: 4px;
+  padding: 10px 2px;
+  display: flex;
+  height: 30px;
+`
+const ToolboxItem = styled.div`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  border-right: ${props => props.bar ? '1px solid #dce0e0' : 'none'};
+  cursor: pointer;
+
+  &:hover {
+    background-color: #dce0e0;
+  }
+`
+const ToolboxSeparator = styled.div`
+  border-right: 1px solid #dce0e0;
+  margin: -10px 0;
+`
+
+export default connect(
+  state => ({}),
+  dispatch => ({
+    setDuration: ((duration) => dispatch(setDuration(duration))),
+    setAccidental: ((accidental) => dispatch(setAccidental(accidental))),
+  }),
+)(Toolbox)
