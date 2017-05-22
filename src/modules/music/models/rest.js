8eb4603c3018e72289593c475b8c461357f87652
Caio Kinzel Filho <caiokf@gmail.com>
2017-05-22 17:50:47 +0300

    extracting note selection handling from artist

--- src/modules/music/models/rest.js
+++ src/modules/music/models/rest.js
@@ -3,14 +3,14 @@
 import StaveElement from 'modules/music/models/stave.element'
 
 export default class Rest extends StaveElement {
-  visible: boolean = true
+  visible: boolean = true;
 
   invisible(): Rest {
     this.visible = false
     return this
   }
 
-  toString() {
+  toString(): string {
     const rest = this.visible ? '##' : '#99#'
     return `:${this.duration} ${rest}`
   }
