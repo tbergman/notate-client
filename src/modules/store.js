ec6fea54855a25c32f871af6daf11e0374007324
Caio Kinzel Filho <caiokf@gmail.com>
2017-06-15 16:57:33 +0300

    removing redux logger

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,7 +1,6 @@
 // @flow
 import { Iterable } from 'immutable'
 import { applyMiddleware, createStore, compose } from 'redux'
-import { createLogger } from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import { persistStore, autoRehydrate } from 'redux-persist'
 import immutableTransform from 'redux-persist-transform-immutable'
@@ -11,24 +10,8 @@ import rootSaga from 'modules/sagas'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const immutableJsLogger = createLogger({
-  stateTransformer: (state) => {
-    const newState = {}
-
-    Object.keys(state).forEach((x) => {
-      if (Iterable.isIterable(state[x])) {
-        newState[x] = state[x].toJS()
-      } else {
-        newState[x] = state[x]
-      }
-    })
-
-    return newState
-  },
-})
-
 const sagaMiddleware = createSagaMiddleware()
-const middleware = applyMiddleware(immutableJsLogger, sagaMiddleware)
+const middleware = applyMiddleware(sagaMiddleware)
 const store = composeEnhancers(
   middleware,
   autoRehydrate(),
